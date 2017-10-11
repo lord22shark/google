726e20b96f5491ed052f2c2fb60d87a4a02e7a92
Jaime Daniel Corrêa Mendes <jaimedaniel@gmail.com>
2017-10-11 14:50:32 -0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # google
 A Facade to use Google Authentication API
+
+This is a facade for using gapi. I consider myself, the available examples a little bit boring and less reusable... not sou much intuitive. For this I wrote this file to help using gapi for authentication and so on in another projects.
+
+> REMEMBER (for me [LOL]) Add the whole process (or link to a how to) of filling the Google requirements, step by step.
+
+## TODOS
+1. Apply JavaScript module properties, package.json, et coetera
+1. Add building routines
+
+## Usage
+
+```javascript
+
+// Creating...
+var foo = new Google({
+	apiKey: '...',
+	discoveryDocs: [
+		'https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest',
+		'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+		'https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest',
+		'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
+	],
+	clientId: '...',
+	scope: 'https://www.googleapis.com/auth/admin.directory.user.readonly https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/gmail.send profile email',
+}, function (user, authorized, authorizationToken) {
+
+	if (authorized !== undefined && authorizationToken !== undefined) {
+
+		gapi.client.directory.users.list({
+			'customer': 'my_customer',
+			'maxResults': 100,
+			'orderBy': 'email'
+		}).then(function (response) {
+
+			if (response.result && response.result.users) {
+
+				response.result.users.forEach(function (user) {
+
+					console.log(user.name.fullName);
+
+				});
+
+			}
+
+		});
+
+	}
+
+});
+
+// Using (when needed)...
+
+/**
+ * Returns the GoogleAuthInstance (Object)
+ */
+foo.getAuthInstance();
+
+/**
+ *  Returns the authenticated user (Object)
+ */
+foo.getUser();
+
+/**
+ * Returns the authorization token (String)
+ */
+foo.getAuthorizationToken();
+
+/**
+ * Returns if scope is authorized or not (Boolean)
+ */
+foo.hasGrantedScopes();
+
+// Disconnecting...
+
+/**
+ * deAuthenticate - true for use authInstance.disconnect and revoke permissions
+ * deAuthenticate - false for use authInstance.signOut and just logout
+ */
+foo.disconnect(deAuthenticate, function () {
+
+	console.log('Disconnected out Signed out');
+
+});
+```
---
+++ src/index.js
@@ -0,0 +1,211 @@
+(function (__window) {
+
+	const Google = function (configuration, __callback) {
+
+		if ((!configuration) || !(configuration instanceof Object)) {
+
+			throw new Error('Google API Wrapper - "configuration" must be defined!');
+
+		}
+
+		if ((!configuration.apiKey) || !(typeof(configuration.apiKey) === 'string') || (configuration.apiKey === '')) {
+
+			throw new Error('Google API Wrapper - "apiKey" must be defined!');
+
+		}
+
+		if ((!configuration.discoveryDocs) || !(configuration.discoveryDocs instanceof Array) || (configuration.discoveryDocs.length === 0)) {
+
+			throw new Error('Google API Wrapper - "discoveryDocs" must be a defined array!');
+
+		}
+
+		if ((!configuration.clientId) || !(typeof(configuration.clientId) === 'string') || (configuration.clientId === '')) {
+
+			throw new Error('Google API Wrapper - "clientId" must be defined!');
+
+		}
+
+		if ((!configuration.scope) || !(typeof(configuration.scope) === 'string') || (configuration.scope === '')) {
+
+			throw new Error('Google API Wrapper - "scope" must be defined!');
+
+		}
+
+		const thiz = this;
+
+		/**
+		 *
+		 */
+		this.element = document.createElement('script');
+
+		this.element.type = 'text/javascript';
+		this.element.async = true;
+		this.element.defer = true;
+		this.element.src = 'https://apis.google.com/js/api.js';
+
+		/**
+		 *
+		 */
+		this.element.onload = function () {
+
+			gapi.load('client:auth2', function () {
+
+				gapi.client.init({
+					'apiKey': configuration.apiKey,
+					'discoveryDocs': configuration.discoveryDocs,
+					'clientId': configuration.clientId,
+					'scope': configuration.scope
+				}).then(function () {
+
+					thiz.googleAuthInstance = gapi.auth2.getAuthInstance();
+
+					// Listen for sign-in state changes.
+					// The callback function must be a global named function
+					thiz.googleAuthInstance.isSignedIn.listen(onUpdateGoogleSignInStatus);
+
+					thiz.setSigninStatus();
+
+				});
+
+			}.bind(thiz));
+
+		};
+
+		/**
+		 *
+		 */
+		this.element.onreadystatechange = function () {
+
+			if (this.readyState === 'complete') {
+
+				this.onload();
+
+			}
+
+		};
+
+		/**
+		 *
+		 */
+		this.setSigninStatus = function (isSignedIn) {
+
+			if ((isSignedIn === true) || (isSignedIn === undefined)) {
+
+				this.user = this.googleAuthInstance.currentUser.get();
+
+				this.authorized = this.user.hasGrantedScopes(configuration.scope);
+
+				this.authorizationToken = (this.authorized) ? this.user.getAuthResponse().access_token : null;
+
+				if (!this.authorized) {
+
+					this.googleAuthInstance.signIn().then(function (authenticatedUser) {
+
+						__callback(this.user, this.authorized, this.authorizationToken);
+
+					}.bind(this)).catch(function (error) {
+
+						__callback(error);
+
+					});
+
+				} else {
+
+					__callback(this.user, this.authorized, this.authorizationToken);
+
+				}
+
+			}
+
+		};
+
+		/**
+		 *
+		 */
+		this.getAuthInstance = function () {
+
+			return this.googleAuthInstance || null;
+
+		};
+
+		/**
+		 *
+		 */
+		this.getUser = function () {
+
+			return this.user || null;
+
+		};
+
+		/**
+		 *
+		 */
+		this.getAuthorizationToken = function () {
+
+			return this.authorizationToken;
+
+		};
+
+		/**
+		 *
+		 */
+		this.hasGrantedScopes = function () {
+
+			return this.authorized;
+
+		};
+
+		/**
+		 *
+		 */
+		this.disconnect = function (deAuthenticate, callback) {
+
+			if (this.googleAuthInstance.isSignedIn.get()) {
+
+				if (deAuthenticate === true) {
+
+					this.googleAuthInstance.disconnect().then(function () {
+
+						if ((callback) && (callback instanceof Function)) {
+
+							callback(true);
+
+						}
+
+					});
+
+				} else {
+
+					this.googleAuthInstance.signOut().then(function () {
+
+						if ((callback) && (callback instanceof Function)) {
+
+							callback(false);
+
+						}
+
+					});
+
+				}
+
+			}
+
+		};
+
+		/**
+		 *
+		 */
+		 __window.onUpdateGoogleSignInStatus = function onUpdateGoogleSignInStatus (isSignedIn) {
+
+			this.setSigninStatus(isSignedIn);
+
+		}.bind(this);
+
+		document.body.appendChild(this.element);
+
+	};
+
+	__window.Google = Google;
+
+})(window);
